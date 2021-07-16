53a8f3040ca0770a82ca4128d3b7f459d442f256
jeremyknudson <134901jeremyknudson@users.noreply.github.com>
2021-07-16 16:48:18 -0500

    [F] - Changing name to main.js

---
+++ main.js
@@ -0,0 +1,56 @@
+var CHA = (function () {
+
+    function helloWorld() {
+        console.log("Hello World!");
+    }
+
+    function matrixSingleAnswer()
+    {
+        // Global Variables: These are constant
+        var qId = "QR~" + this.questionId + "~";
+        var embeddedData;
+        var previousAnswers = [];
+        var getChoice;
+
+
+        // Local Variables: These change per question
+        // questionNumber = "PYQID149_"; // PreviousYear survey data pointer
+        var questionNumber = "PY" + this.questionId; // PreviousYear survey data pointer
+
+
+        // Exit script if question has already been answered
+        if (this.getSelectedChoices().length !== 0) {
+            return;
+        }
+
+
+        // Build Choice Array
+        try {
+            for (var i = 1; i < 100; i++) // Notice i starts at 1, not 0!
+            {
+                getChoice = document.getElementById(qId + i);
+                if (getChoice !== null) {
+                    previousAnswers.push(i);
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+        catch (error) {
+            console.log("Inside catch block! Array failed to populate.");
+        }
+
+
+        // Handle Embedded Data
+        previousAnswers.forEach(function () {
+            console.log(questionNumber);
+            embeddedData = Qualtrics.SurveyEngine.getEmbeddedData(questionNumber);
+
+            if (embeddedData !== null) {
+                document.getElementById(qId + embeddedData).checked = true;
+            }
+        });   
+    }
+
+}());
