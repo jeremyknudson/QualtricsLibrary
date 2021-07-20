0c4f8ec752ec159e0fa9d2709fe91ad704c7a613
jeremyknudson <134901jeremyknudson@users.noreply.github.com>
2021-07-20 10:25:03 -0500

    testing Dave's mixin pattern

--- HW.js
+++ HW.js
@@ -1,11 +1,55 @@
-var CHA = (function () {
-
-    function helloWorld() {
-        console.log("Hello World!");
+var CHA = {
+    IsAnswered: function () {
+        if (this.getSelectedChoices().length !== 0) {
+            console.log("Already answered");
+            return;
+        }
+        else {
+            console.log("Setting answer . . . .");
+        };
     }
+};
 
-    function goodbyeWorld() {
-        console.log("Goodbye World!");
-    }
 
-}());
+
+//// Stacked Locally Scoped Object Literal
+//var CHA = (function () {
+
+//    var publicFunctions = {
+//        HW: function helloWorld() {
+//            console.log("Hello World!");
+//        },
+//        GW: function goodbyeWorld() {
+//            console.log("Goodbye World!");
+//        }
+//    }
+
+//    return publicFunctions;
+//}());
+
+//CHA.HW();
+//CHA.GW();
+
+
+
+
+// Revealing Method: really like for this library with multiple, somewhat lengthy functions
+//var CHA = (function () {
+
+//    var HW = function helloWorld() {
+//        console.log("Hello World!");
+//    };
+
+//    var GW = function goodbyeWorld() {
+//        console.log("Goodbye World!");
+//    };
+
+
+//    return {
+//        HW: HW,
+//        GW: GW
+//    }
+//}());
+
+//CHA.HW();
+//CHA.GW();
